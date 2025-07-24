92333eb8c02a1edfe18c12dd409e7c0b17721fe0
Bao Nguyen <bnguyen@ctacorp.com>
2025-07-24 13:18:51 -0400

    update incomplete flags 4

--- js/familytree.js
+++ js/familytree.js
@@ -3,7 +3,19 @@ fetch('js/data.json')
   .then(data => {
     new FamilyTree(document.getElementById("tree"), {
       nodeBinding: {
-        field_0: "name"
+        field_0: "name",
+        field_1: "title"
+      },
+      menu: {
+          pdf: { text: "Export PDF" },
+          png: { text: "Export PNG" },
+          svg: { text: "Export SVG" },
+          json: { text: "Export JSON" }
+      },
+      nodeMenu: {
+          pdf: { text: "Export PDF" },
+          png: { text: "Export PNG" },
+          svg: { text: "Export SVG" }
       },
       nodes: data
     });
