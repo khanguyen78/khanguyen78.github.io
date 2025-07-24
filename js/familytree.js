12694619e618d996d58bace140f0f5087e71cd79
Bao Nguyen <bnguyen@ctacorp.com>
2025-07-24 12:59:17 -0400

    testing title field 3

--- js/familytree.js
+++ js/familytree.js
@@ -3,19 +3,7 @@ fetch('js/data.json')
   .then(data => {
     new FamilyTree(document.getElementById("tree"), {
       nodeBinding: {
-        field_0: "name",
-        field_1: "title"
-      },
-      menu: {
-          pdf: { text: "Export PDF" },
-          png: { text: "Export PNG" },
-          svg: { text: "Export SVG" },
-          json: { text: "Export JSON" }
-      },
-      nodeMenu: {
-          pdf: { text: "Export PDF" },
-          png: { text: "Export PNG" },
-          svg: { text: "Export SVG" }
+        field_0: "name"
       },
       nodes: data
     });
